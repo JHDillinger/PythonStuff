522c64bc319c7d22ecc0b1145732db211bc262e3
JHDillinger <armin.bernstetter@gmail.com>
2017-09-01 18:12:18 +0200

    started roguelike tutorial http://rogueliketutorials.com/libtcod/1

--- test/helloworld.py
+++ test/helloworld.py
@@ -1,6 +1,6 @@
 name = input("What's your name? ")
 age = int(input("Age? "))
 if age <18:
-    print("Hey," + name + "you're not old enough!")
+    print("Hey, " + name + " you're not old enough!")
 else:
     print("Here's your beer, "+name+"!")
